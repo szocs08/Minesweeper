0573c122758cb368db6b5ec1bea541429aea3659
szocs08 <szgabor08@gmail.com>
2016-05-20 18:33:10 +0200

    Difficulty javadoc

--- src/main/java/minesweeper/model/Difficulty.java
+++ src/main/java/minesweeper/model/Difficulty.java
@@ -1,7 +1,27 @@
 package minesweeper.model;
 
 
+/**
+ * A nehézségi szinteket reprezentáló {@code enum}
+ * @author Gábor
+ *
+ */
 public enum Difficulty {
-	EASY, MEDIUM, HARD,CUSTOM;
+	/**
+	 * A könnyű nehézségi szintet reprezentáló érték
+	 */
+	EASY, 
+	/**A közepes nehézségi szintet reprezentáló érték
+	 * 
+	 */
+	MEDIUM,
+	/**
+	 * A nehéz nehézségi szintet reprezentáló érték
+	 */
+	HARD,
+	/**
+	 * Az egyedi nehézségi szintet reprezentáló érték
+	 */
+	CUSTOM;
 
 }
